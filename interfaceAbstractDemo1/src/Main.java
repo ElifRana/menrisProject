cdc38c7da078ce27eadbdb08551db3a2d0358870
elifr <elifr@192.168.1.31>
2021-05-07 23:41:10 +0300

    Mernis Project

--- interfaceAbstractDemo1/src/Main.java
+++ interfaceAbstractDemo1/src/Main.java
@@ -6,7 +6,7 @@ import concrete.StarbucksCustomerManager;
 import entities.Customer;
 
 public class Main {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 		
 		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
 		
